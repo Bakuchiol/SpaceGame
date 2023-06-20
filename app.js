40b8e73c18510827fea6a0431804935ba6252403
Lynette <lynettecargo@Lynettes-Air.home>
2023-06-20 14:05:25 -0500

    fixed accuracy function

--- app.js
+++ app.js
@@ -41,55 +41,87 @@ function getRandomArbitrary(min, max) {
   return Math.random() * (max - min) + min;
 }
  */
+/**********************random number for alien powweerrr */
+const alienRandomizer = (min, max) => {
+    let randomNumber = Math.floor(Math.random() * (min - max) + min)
+    return randomNumber
+}
+/**********************random number for alien accuracy */
+const alienAccuracy = (min, max) => {
+    return (Math.random() * ((min - max) + min)).toFixed(2)
+}
 // ONE OBJECT? OR DIFFERENT OBJECTS??
 const alienSpaceships = [
     alien1 = {
         name: "The Hocotate Ship",
         hull: alienRandomizer(6, 3),
         firepower: alienRandomizer(4, 2),
-        accuracy: alienRandomizer(.8, .6)
+        accuracy: alienAccuracy(.8, .6)
     },
     alien2 = {
         name: "S.S. Dolphin",
         hull: alienRandomizer(6, 3),
         firepower: alienRandomizer(4, 2),
-        accuracy: alienRandomizer(.8, .6)
+        accuracy: alienAccuracy(.8, .6)
     },
     alien3 = {
         name: "S.S. Drake",
         hull: alienRandomizer(6, 3),
         firepower: alienRandomizer(4, 2),
-        accuracy: alienRandomizer(.8, .6)
+        accuracy: alienAccuracy(.8, .6)
     },
     alien4 ={
         name: "Research Pod",
         hull: alienRandomizer(6, 3),
         firepower: alienRandomizer(4, 2),
-        accuracy: alienRandomizer(.8 ,.6)
+        accuracy: alienAccuracy(.8, .6)
     },
     alien5 ={
         name: "S.S. Dolphin II",
         hull: alienRandomizer(6, 3),
         firepower: alienRandomizer(4, 2),
-        accuracy: alienRandomizer(.8, .6)
+        accuracy: alienAccuracy(.8, .6)
     },
     alien6 ={
         name: "The Arwing",
         hull: alienRandomizer(6, 3),
         firepower: alienRandomizer(4, 2),
-        accuracy: alienRandomizer(.8, .6)
+        accuracy: alienAccuracy(.8, .6)
     }
 ]
 console.log(alienSpaceships)
 
-/**********************random number for alien powweerrr */
-const alienRandomizer = (min, max) => {
-    let randomNumber = Math.floor(Math.random() * (min - max) + min)
-    return randomNumber
-}
 
 /************************************************************************ FUNCTIONS */
 
-// ATTACK
-//  WHILE LOOP
-/* while we attack,  */
+/***** ATTACK */
+//always attack first then alien
+//click attack --> commence game
+// alienSpaceships.forEach() {
+//     while (spaceShip.hull > 0) {
+//         spaceShip
+//         if () {
+
+//         }
+//     }
+// }
+const attackAlienShip = () => {
+    alienSpaceships.forEach(alien => functionName(alien))
+
+}
+
+
+//if own ship survive, attack again
+
+
+//if alien survive, alien ship attacks again
+
+//if alien ship destroyed, can ATTACK NEXT SHIP OR RETREAT
+
+/******* RETREAT */
+// retreat: only visible after defeating ship
+// otherwise not visible at beginning ONLY START AND ATTACK
+// RETREAT === GAME OVER
+
+// WIN = ALIEN SHIPS DESTROYED
+// LOSE = OWN SHIP DESTROYED
