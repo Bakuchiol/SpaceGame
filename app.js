4e793fea8e9219096f5d006c745025f1d562c7fa
Lynette <lynettecargo@Lynettes-Air.home>
2023-06-20 16:13:28 -0500

    added random number functions to aliens

--- app.js
+++ app.js
@@ -4,6 +4,16 @@
 // firepower - amount of damage done to hull
 // accuracy - chance between 0 -1 [math.random]
 
+/**********************random number for alien powweerrr */
+const alienRandomizer = (min, max) => {
+    let randomNumber = Math.floor(Math.random() * (min - max) + min)
+    return randomNumber
+}
+/**********************random number for alien accuracy */
+const alienAccuracy = (min, max) => {
+    return (Math.random() * ((min - max) + min)).toFixed(2)
+}
+
 // try factory
 class SpaceShip {
     constructor(name, hull, firepower, accuracy){
@@ -13,6 +23,32 @@ class SpaceShip {
         this.accuracy = accuracy
     }
 }
+
+// player ship
+class PlayerShip extends SpaceShip {
+    constructor(name, hull, firepower, accuracy){
+        super(name, hull, firepower, accuracy)
+        this.name = name,
+        this.hull = hull,
+        this.firepower = firepower,
+        this.accuracy = accuracy
+    }
+}
+
+// alien ships
+class AlienShip extends SpaceShip {
+    constructor(name, hull, firepower, accuracy){
+        super(name, hull, firepower, accuracy)
+        this.name = name,
+        // hull: 3 - 6
+        this.hull = alienRandomizer(3, 6),
+        // firepower: 2 - 4
+        this.firepower = alienRandomizer(2, 4),
+        // accuracy: .6 - .8
+        this.accuracy = alienAccuracy(.6, .8)
+    }
+}
+console.log(alienAccuracy(.8, .6))
 
 // Ship: USS Assembly
 //can only attack in order
@@ -48,15 +84,6 @@ function getRandomArbitrary(min, max) {
   return Math.random() * (max - min) + min;
 }
  */
-/**********************random number for alien powweerrr */
-const alienRandomizer = (min, max) => {
-    let randomNumber = Math.floor(Math.random() * (min - max) + min)
-    return randomNumber
-}
-/**********************random number for alien accuracy */
-const alienAccuracy = (min, max) => {
-    return (Math.random() * ((min - max) + min)).toFixed(2)
-}
 // ONE OBJECT? OR DIFFERENT OBJECTS??
 const alienSpaceships = [
     alien1 = {
@@ -96,69 +123,66 @@ const alienSpaceships = [
         accuracy: alienAccuracy(.8, .6)
     }
 ]
-// console.log(alienSpaceships)
-const enemies = [alienSpaceships]
 
 /************************** VARIABLES */
 // top screen
-const playerHealth = document.querySelector('.playerHealth');
-const roundCount = document.querySelector('.roundCount');
-const alienHealth = document.querySelector('.alienHealth');
+// const playerHealth = document.querySelector('.playerHealth');
+// const roundCount = document.querySelector('.roundCount');
+// const alienHealth = document.querySelector('.alienHealth');
 
-// main screen
-const startTitle = document.querySelector('.startTitle')
-const playerShip = document.querySelector('.playerShip');
-const alienShip = document.querySelector('.alienShip');
-
-
-// footer
-const start = document.querySelector('.start');
-const attack = document.querySelector('.attack');
-const retreat = document.querySelector('.retreat');
-
-/************************************************************************ FUNCTIONS */
-// start, health appears, alien ship appears
-// start.addEventListener('click', alienAppear);
-const shipsAppear = () => {
-    playerShip.classList.toggle('ussAssembly')
-    alienShip.classList.toggle('ussAlien')
-    startTitle.classList.toggle('pressStartGone')
-}
-
-/************************************************** ATTACK */
-//always attack first then alien
-//click attack --> commence game
-//if own ship survive, attack again
-//if alien survive, alien ship attacks again
-//if alien ship destroyed, can ATTACK NEXT SHIP OR RETREAT
-
-const 
+// // main screen
+// const startTitle = document.querySelector('.startTitle')
+// const playerShip = document.querySelector('.playerShip');
+// const alienShip = document.querySelector('.alienShip');
 
 
+// // footer
+// const start = document.querySelector('.start');
+// const attack = document.querySelector('.attack');
+// const retreat = document.querySelector('.retreat');
 
-
-
-
-
-
-/******* RETREAT */
-// retreat: only visible after defeating ship
-// otherwise not visible at beginning ONLY START AND ATTACK
-// RETREAT === GAME OVER
-
-// WIN = ALIEN SHIPS DESTROYED
-// LOSE = OWN SHIP DESTROYED
-
-const startGame = () => {
-    if(player1.isMyTurn === true){
-        attackPlayer(player2)
-    }else{
-        attackPlayer(player1)
-    }
-}
-
-// restart?
-// const restart = () => {
-//     //health = 0
-//     //function to go back to screen/start
+// /************************************************************************ FUNCTIONS */
+// // start, health appears, alien ship appears
+// // start.addEventListener('click', alienAppear);
+// const shipsAppear = () => {
+//     playerShip.classList.toggle('ussAssembly')
+//     alienShip.classList.toggle('ussAlien')
+//     startTitle.classList.toggle('pressStartGone')
 // }
+
+// /************************************************** ATTACK */
+// //always attack first then alien
+// //click attack --> commence game
+// //if own ship survive, attack again
+// //if alien survive, alien ship attacks again
+// //if alien ship destroyed, can ATTACK NEXT SHIP OR RETREAT
+
+
+
+
+
+
+
+
+
+// /******* RETREAT */
+// // retreat: only visible after defeating ship
+// // otherwise not visible at beginning ONLY START AND ATTACK
+// // RETREAT === GAME OVER
+
+// // WIN = ALIEN SHIPS DESTROYED
+// // LOSE = OWN SHIP DESTROYED
+
+// const startGame = () => {
+//     if(player1.isMyTurn === true){
+//         attackPlayer(player2)
+//     }else{
+//         attackPlayer(player1)
+//     }
+// }
+
+// // restart?
+// // const restart = () => {
+// //     //health = 0
+// //     //function to go back to screen/start
+// // }
